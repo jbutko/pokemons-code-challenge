75e584425deac6b77f30dda2fdde0719ade4b087
Jozef Butko <jbutko@gmail.com>
2024-03-21 10:58:42 +0100

    refactor(PokemonsList): separate PokemonModal component

---
+++ frontend/src/components/organisms/PokemonsList/components/PokemonModal/PokemonModal.tsx
@@ -0,0 +1,26 @@
+import { Modal } from '@/components/molecules/Modal/Modal'
+import { PokemonTile } from '@/components/molecules/PokemonTile/PokemonTile'
+import { TNullable } from '@/types/common.types'
+import { Pokemon } from '@/types/pokemons'
+
+type TProps = {
+  data: TNullable<Pokemon>
+  onClose: () => void
+  launcherButtonRef: React.RefObject<HTMLButtonElement>
+}
+
+export const PokemonModal: React.FC<TProps> = ({ data, onClose, launcherButtonRef }) => (
+  <Modal
+    aria-label="Pokemon details"
+    id="pokemon-details"
+    headerProps={{
+      title: 'Pokemon details',
+    }}
+    launcherButtonRef={launcherButtonRef}
+    onClose={onClose}
+    onSubmit={onClose}
+    open={!!data}
+  >
+    {!!data && <PokemonTile data={data} key={data.id} showDetails showFavorite={false} showSimilar showSound />}
+  </Modal>
+)
