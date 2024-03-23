7bc6be755cebc86bffc1661839cf5768e7294335
Jozef Butko <jbutko@gmail.com>
2024-03-23 01:28:16 +0100

    refactor(PokemonEvolutions): css styling

--- frontend/src/components/molecules/PokemonEvolutions/PokemonEvolutions.tsx
+++ frontend/src/components/molecules/PokemonEvolutions/PokemonEvolutions.tsx
@@ -2,6 +2,7 @@ import { Column, Grid } from '@carbon/react'
 import { PokemonTile } from '@/components/molecules/PokemonTile/PokemonTile'
 import { Pokemon } from '@/types/pokemons'
 import { Title } from '@/components/molecules/Title/Title'
+import styles from './pokemon-evolutions.module.scss'
 
 type TProps = {
   data: Pokemon['evolutions']
@@ -9,12 +10,12 @@ type TProps = {
 
 export const PokemonEvolutions: React.FC<TProps> = ({ data }) => {
   return (
-    <Grid>
-      <Column lg={16}>
+    <Grid condensed className={styles.container}>
+      <Column lg={16} className={styles.title}>
         <Title level={3}>Evolutions</Title>
       </Column>
       {data.map((evolution) => (
-        <Column lg={2} key={evolution.id}>
+        <Column lg={4} key={evolution.id}>
           <PokemonTile data={evolution} showFavorite={false} />
         </Column>
       ))}
