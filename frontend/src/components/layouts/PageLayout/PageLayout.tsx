9cfe55caf89efede4d8aba9436a747c837fe7d3d
Jozef Butko <jbutko@gmail.com>
2024-03-24 17:38:45 +0100

    feat(css): page layout styling

--- frontend/src/components/layouts/PageLayout/PageLayout.tsx
+++ frontend/src/components/layouts/PageLayout/PageLayout.tsx
@@ -3,11 +3,12 @@
 import { TPropsWithChildren } from '@/types/common.types'
 import { Column, Grid, GridSettings } from '@carbon/react'
 import { GridProps } from '@carbon/react/lib/components/Grid/GridTypes'
+import styles from './page-layout.module.scss'
 
 export const PageLayout: TPropsWithChildren<GridProps<React.ElementType>> = ({ children, ...gridProps }) => (
   <Grid condensed {...gridProps}>
     <GridSettings mode="css-grid">
-      <Column sm={4} md={8} lg={16}>
+      <Column sm={4} md={8} lg={16} className={styles.container}>
         {children}
       </Column>
     </GridSettings>
